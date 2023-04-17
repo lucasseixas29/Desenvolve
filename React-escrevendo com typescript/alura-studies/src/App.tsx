e6b3a7f72214704c728de94d3c8754c9207769d0
Lucas Seixas <lucasseixas29@hotmail.com>
2023-04-16 21:28:04 -0300

    estudando react

---
+++ React-escrevendo com typescript/alura-studies/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+import Formulario from './components/Formulario'
+import Lista from "./components/Lista"
+import style from "./App.module.scss";
+
+function App() {
+
+  
+    return (
+    <div className={style.AppStyle}>
+      <Formulario/>
+      <Lista/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-escrevendo com typescript/alura-studies/src/components/Formulario/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Botao from "../Botao"
+import style from "./Formulario.module.scss";
+
+function Formulario () {
+    
+return (
+
+<form className={style.novaTarefa}>
+    <div className={style.inputContainer}>
+        <label htmlFor="tarefa">
+            Adicione um novo estudo
+        </label>
+        <input 
+        type="text" 
+        name="tarefa"
+        id="tarefa"
+        placeholder="O que você quer estudar?"
+        required
+        />
+    </div>
+    <div className={style.inputContainer}>
+        <label>
+            Tempo
+        </label>
+        <input 
+        type="time"
+        step="1"
+        name="tempo"
+        min="00:00:00"
+        max="01:30:00"
+        required
+        />
+    </div>
+    <Botao/>
+</form>
+
+)
+
+}
+
+
+export default Formulario;
